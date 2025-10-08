7f9570e8266d106e6059a2384ec43a84fd984567
Aldi Julse Makruf <158381668+aldijm17@users.noreply.github.com>
2025-10-09 00:18:30 +0700

    update tampilan

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
+// --- FUNGSI UNTUK MENU MOBILE ---
 const mobileMenuButton = document.getElementById('mobile-menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
 
 mobileMenuButton.addEventListener('click', () => {
-    // Toggle class 'hidden' untuk menampilkan/menyembunyikan menu
     mobileMenu.classList.toggle('hidden');
+});
+
+// --- FUNGSI UNTUK ANIMASI FADE-IN SAAT SCROLL ---
+const scrollElements = document.querySelectorAll(".scroll-reveal");
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+    return (
+        elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add("animate");
+};
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        }
+    })
+}
+
+window.addEventListener("scroll", () => { 
+    handleScrollAnimation();
 });
